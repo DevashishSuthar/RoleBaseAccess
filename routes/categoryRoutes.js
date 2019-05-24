abe6950571577da223d5b3a4078d0e7fe327ea5d
DevashishSuthar <44345744+DevashishSuthar@users.noreply.github.com>
2019-05-24 15:47:51 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const categoryController = require('../controller/categoryController');
 
-// //for creating ==>
+//for creating ==>
 router.post('/create-category', categoryController.authController, categoryController.createCategory);
-// //To get specific id ==>
+//To get specific id ==>
 router.get('/view-category/:id', categoryController.authController, categoryController.viewCategory);
-// //for update ==>
-// router.put('/update-category/:id', categoryController.updateCategory);
-// //for delete ==>
-// router.delete('/delete-category/:id', categoryController.deleteCategory);
-// //To get all collections ==>
-// router.get('/get-all-category', categoryController.getAllCategory);
+//for update ==>
+router.put('/update-category/:id', categoryController.updateCategory);
+//for delete ==>
+router.delete('/delete-category/:id', categoryController.deleteCategory);
+//To get all collections ==>
+router.get('/get-all-category', categoryController.getAllCategory);
 
 module.exports = router;
